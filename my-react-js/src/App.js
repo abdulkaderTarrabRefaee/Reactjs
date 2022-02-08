0e9cca0abfd8d13fb20134e404fafdb6ec758569
Abdulkader tarrab <Abdulkadertarrabrefaee@gmail.com>
2022-02-08 16:45:48 +0300

    crate project

---
+++ my-react-js/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../assert/css/custom.css'
+import '../../asset/css/custom.css'
+import {Container,Row , Col ,Button} from 'react-bootstrap'
+import '../../asset/css/Bootstrap.min.css'
+function App() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-js/src/componets/TobBanner.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+export class TobBanner extends Component {
+    render() {
+        return (
+        <Fragment>
+
+
+        </Fragment>
+        )
+    }
+}
+
+export default TobBanner;
